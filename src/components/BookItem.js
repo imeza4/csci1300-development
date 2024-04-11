81322434a84f4a5fd578f8e6a7aa13f131d7392d
Isabelle Meza <isabellemeza@eugene.devices.brown.edu>
2024-04-10 23:28:20 -0400

    complete

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -6,10 +6,9 @@ export default function BookItems(props) {
     return (
       <div className="BookItems">
         <div>
-        <img alt="book item image" src={props.image}/>
         </div>
         <h1>{props.name}</h1> 
-        <p>{props.genre}, {props.pages}</p>
+        <p>{props.genre}, {props.pages} pages</p>
         <p class="price">{props.price}</p>
         <BookButton cartItems={props.cart} name = {props.name} price={props.price} updateCart={props.updateCart} updatePrice={props.updatePrice} currPrice={props.currPrice}/>
       </div>
